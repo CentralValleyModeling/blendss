c55e5ddede2b4be53ffe406a3a6c124530b5d81e
Zachary Roy <zachary.roy@water.ca.gov>
2024-06-27 12:59:12 -0700

    Fix import order

--- src/blendss/__init__.py
+++ src/blendss/__init__.py
@@ -1,10 +1,11 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from .blend import blend
-from .cli import cli
-
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
     # blendss not installed, likely developer mode
     __version__ = None
+
+
+from .blend import blend
+from .cli import cli
